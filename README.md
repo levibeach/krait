8d957f3b18792363927e7d70bd8102f1b23aa435
Levi Beach <levimbeach@gmail.com>
2024-08-05 19:36:48 -0400

    add basic looper

---
+++ README.md
@@ -0,0 +1,51 @@
+# KRAIT
+
+![Red-headed krait](https://upload.wikimedia.org/wikipedia/commons/5/55/Red-headed_Krait.jpg)
+
+Krait is a small command line application that records and plays MIDI messages.
+
+**WARNING:** This is a work-in-progress and was created fairly quickly so use at your own risk.
+
+## Install & Run
+
+To get Krait running in the terminal, first makes ure you you have a Node and NPM installed.
+
+```bash
+git clone https://github.com/levibeach/krait.git
+cd krai
+npm install
+npm run start
+# OR
+node index
+```
+
+## Usages
+
+### Arming & Recording
+
+The idea is to quickly arm and record loops:
+
+1. Select a loop by pressing any number key 1-9.
+2. The loop you selected will highlight indicating that it is ready to record (armed).
+2. As soon as a MIDI signal is detected from the MIDI in it will start recording messages to the loop.
+3. When you want to stop the recording, just press the same number key again.
+
+After you have stopped recording the loop will immediately start playing so that there is virtually no pause when looping in a live session.
+
+### Overdubbing
+
+If you want to add to an existing loop:
+
+1. Select a loop you by pressing the number key.
+2. Play something!
+3. Press the loop number key again.
+
+### Pausing, Replaying, & Deleting
+
+While holding `Shift`:
+- Press a number once to pause/replay that loop.
+- Double-press a number while it is playing to delete that loop.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,208 @@
+// time is an illusion
+
+const blessed = require('blessed')
+const midi = require('midi')
+const midiIn = new midi.Input()
+const midiOut = new midi.Output()
+const midiMap = require('./midimap.js')
+
+const loops = new Map()
+let armed = null
+let recording = false
+let overdub = false
+let output = ''
+let midiRate = 5 // ¯\_(ツ)_/¯
+let lastKeyPressTime = 0
+const doubleTapThreshold = 300
+// probably a better way to do this
+// but mapping the shift keys works for now…
+const shiftKeys = {
+  '!':1,
+  '@':2,
+  '#':3,
+  '$':4,
+  '%':5,
+  '^':6,
+  '&':7,
+  '*':8,
+  '(':9
+}
+const screen = blessed.screen({
+  fastCSR: true
+})
+const logbook = blessed.log({
+	parent: screen,
+	bottom: 0,
+	left: 0,
+	mouse: false,
+	width: screen.width - 2,
+	height: 5,
+	content: '',
+})
+
+midiIn.openPort(0)
+midiOut.openPort(0)
+
+function log(msg) {
+  output += `${msg}\n`
+  logbook.setText(output)
+}
+
+function recordLoop() {
+  if (!armed) return
+  recording = true
+  armed.label.style.bg = 'red'
+  if (!armed.locked) {
+    armed.frame = 0
+    armed.interval = setInterval(() => {
+      armed.frame++
+    }, midiRate)
+  }
+}
+
+function stopRecord() {
+  recording = false
+  armed.label.style.bg = 'default'
+  if (!armed.loopLength) {
+    armed.loopLength = armed.frame
+    armed.locked = true
+  } else {
+    overdub = true
+  }
+  // clear the recording interval
+  clearInterval(armed.interval)
+  startLoop(armed.id)
+}
+
+function startLoop(lid) {
+  const loop = loops.get(lid)
+  loop.playing = true
+  if (overdub) {
+    overdub = false
+  } else {
+    loop.frame = 0
+  }
+  loop.interval = setInterval(() => {
+		loop.display.setProgress(Math.ceil((loop.frame/loop.loopLength)*100))
+    if (loop.data.has(loop.frame)) {
+      const midiData = loop.data.get(loop.frame)
+      midiData.forEach((item) => {
+        midiOut.sendMessage(item)
+      })
+    }
+    if (loop.frame + 1 > loop.loopLength) {
+      loop.frame = 0
+    } else {
+      loop.frame++
+    }
+  }, midiRate)
+}
+
+function stopLoop(lid) {
+  const loop = loops.get(lid)
+  loop.playing = false
+  clearInterval(loop.interval)
+}
+
+function toggleArmed(lid) {
+  // create a new loop if it doesn't exist
+  if (!loops.has(lid)) {
+    loops.set(lid,{
+      id: lid,
+      name: `${lid}`,
+      frame: null,
+      loopLength: null,
+      locked: false,
+      playing: false,
+      interval: null,
+      data: new Map(),
+      label: blessed.box({
+        parent: screen,
+        top: lid,
+        left: 0,
+        width: 1,
+        height: 1,
+        content: `${lid}`
+      }),
+      display: blessed.ProgressBar({
+        parent: screen,
+        top: lid,
+        left: 2,
+        pch: '—',
+        width: screen.width - 1,
+        height: 1,
+      })
+    })
+  }
+  if (armed) {
+    if (recording) {
+      stopRecord()
+      log(`loop ${armed.id}: ${armed.data.size} events`)
+    }
+    armed.label.style.bg = 'default'
+    armed = null
+  } else {
+    armed = loops.get(lid)
+    armed.label.style.bg = 'green'
+  }
+}
+
+function init() {
+  screen.key([1,2,3,4,5,6,7,8,9], (ch,key) => {
+    toggleArmed(parseInt(ch))
+  })
+
+  screen.key(['!','@','#','$','%','^','&','*','('], (ch, key) => {
+    const k = parseInt(shiftKeys[ch])
+    if (!loops.has(k)) return
+
+    const loop = loops.get(k)
+    const currentTime = new Date().getTime()
+    const timeDifference = currentTime - lastKeyPressTime
+
+    if (timeDifference < doubleTapThreshold && !loop.playing) {
+      loop.label.destroy()
+      loop.display.destroy()
+      log(`deleting loop ${k}`)
+      loops.delete(k)
+      return
+    }
+    lastKeyPressTime = currentTime
+    loop.playing ? stopLoop(k) : startLoop(k)
+    log(`loop ${k} ${loop.playing ? 'restarted' : 'has been paused'}`)
+  })
+
+  // Quit on Escape, q, or Control-C.
+  screen.key(['q', 'escape', 'C-c'], () => {
+    process.exit()
+  })
+
+  // sent note off for all channels
+  screen.key([0], () => {
+    log('turning off all sound')
+    for (let chan = 0; chan < 16; chan++) {
+      midiOut.sendMessage([0xB0 + chan, 123, 0])
+    }
+  })
+
+  log(`midiIn: "${midiIn.getPortName(1)}"`)
+
+  midiIn.on('message', (deltaTime, message) => {
+    // log(`m: ${message} d: ${deltaTime}`)
+    if (Object.keys(midiMap).includes(`${message[0]}`)) {
+      if (armed) {
+        if (!recording) recordLoop()
+        if (armed.data.has(armed.frame)) {
+          const newArr = armed.data.get(armed.frame)
+          newArr.push(message)
+        } else {
+          armed.data.set(armed.frame, [message])
+        }
+      }
+    }
+  })
+
+  setInterval(()=> screen.render(),100)
+}
+
+init()
